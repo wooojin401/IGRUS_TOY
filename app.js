2b022d4e0ef295410c6d5ab9543f1a3226244d53
hyeinkim01 <117064767+hyeinkim01@users.noreply.github.com>
2023-02-02 18:08:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
-console.log('hello')
-console.log(1+1)
+const express = require('express')
+const session = require('express-session')
+const cookieParser=require('cookie-parser');
+const bodyParser = require('body-parser');
+const FileStore = require('session-file-store')(session)
+var template = require('./admin/template.js');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+                                          // 로그인 되어있으면 메인 페이지로 이동시킴
+      res.redirect('/main');
+
+  })
+
+app.get('/main', (req, res) => {
+    
+
+    var html = template.HTML('Welcome',
+      `<hr>
+          <h2>메인페이지</h2>
+          <p><a href="/auth/">수강신청하러가기</a></p>
+          <p><a href="/auth/">시간표 확인하기</a></p>`
+    );
+    res.send(html);
+  })
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
