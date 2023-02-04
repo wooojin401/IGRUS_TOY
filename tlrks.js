073ab9f26aa367d2508495021879c32fb8f425dd
kim-chae-won <117062527+kim-chae-won@users.noreply.github.com>
2023-02-04 18:02:06 +0900

    Create tlrks.js

---
+++ tlrks.js
@@ -0,0 +1,137 @@
+<!doctype html>
+<html>
+	<Head>
+		<style>
+			table, th, td{
+				border: 1px solid black;
+				border-collapse: collapse;
+				text-align: center;
+				width: 100px;
+				height: 40px;
+			}
+			th,td{
+				padding: 20px;
+			}
+			th{
+				text-align: center;
+			}
+
+		</style>
+	</Head>
+	<body>
+		<h1>시간표</h1>
+		<table style="width:100%">
+			<tr>
+				<th></th>
+				<th>월</th>
+				<th>화</th>
+				<th>수</th>
+				<tn>목</tn>
+				<th>금</th>
+			</tr>
+			<tr>
+				<th>1</th>
+				<td></td>
+				<td style = "background : green ;"rowspan = "3">데이터베이스</td>
+				<td style = "background : green ;"rowspan = "3">데이터베이스</td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<th>2</th>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<th>3</th>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<th>4</th>
+				<td></td>
+				<td></td>
+				<td style = "background : blue ;"rowspan = "4">공간분석</td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<th>5</th>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<th>6</th>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<th>7</th>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<th>8</th>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<th>9</th>
+				<td style = "background : yellow;"rowspan = "4">사진측량학</td>
+				<td></td>
+				<td style = "background : yellow;"rowspan = "4">사진측량학</td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<th>10</th>
+				<td style = "background : red ;"rowspan = "3">공간정보 수치해석</td>
+				<td style = "background : red ;"rowspan = "3">공간정보 수치해석</td>
+				<td></td>
+			</tr>
+			<tr>
+				<th>11</th>
+				<td></td>
+			</tr>
+			<tr>
+				<th>12</th>
+				<td></td>
+			</tr>
+			<tr>
+				<th>13</th>
+				<td style = "background : pink ;"rowspan = "3">환경지구정보학</td>
+				<td></td>
+				<td style = "background : pink ;"rowspan = "3">환경지구정보학</td>
+				<td style = "background : blue ;"rowspan = "4">공간분석</td>
+				<td></td>
+			</tr>
+			<tr>
+				<th>14</th>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<th>15</th>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+
+
+
+			
+		</table>
+	</body>
+</html>
